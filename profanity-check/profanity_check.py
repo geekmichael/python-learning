f062696963893016e9b66697f157625a4e01d473
Michael ZHANG <geek.michael@live.com>
2017-03-14 22:12:55 +0800

    Changed the profanity check service to http://www.purgomalum.com

--- profanity-check/profanity_check.py
+++ profanity-check/profanity_check.py
@@ -4,10 +4,10 @@ def read_text():
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
-    check_profanity('shot')
+    check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    connection = urllib.urlopen("http://www.purgomalum.com/service/containsprofanity?text="+text_to_check)
     output = connection.read()
     if "true" in output:
         print("Profanity Alert!")
